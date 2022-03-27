25f74a60ea53188c92c7e7030e03789cbeabbf4c
Justin Ennis <justin@nrc-llc.com>
2022-03-27 15:14:42 -0700

    Functional calculator.

--- IOB Calculator/ContentView.swift
+++ IOB Calculator/ContentView.swift
@@ -9,77 +9,54 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    
+    @State var bg: String = ""
+    @State var tdd: String = ""
+    @State var iobn: String = "0"
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        Form {
+            HStack {
+                Text("Blood Glucose")
+                Spacer()
+                TextField("Blood Glucose", text: $bg)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.numberPad)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+            HStack {
+                Text("Total Daily Dose")
+                Spacer()
+                TextField("Total Daily Dose", text: $tdd)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
             }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            HStack {
+                Spacer()
+                Button(action: {
+                    let bgfloat = Float(bg) ?? 0.0
+                    let tddfloat = Float(tdd) ?? 0.0
+                    let cf = 277700/(bgfloat * tddfloat)
+                    let iobnnumerator = (bgfloat - 100) < 0 ? 0 : bgfloat - 100
+                    iobn = String(format: "%.2f", iobnnumerator / cf)
+                }) {
+                    Text("Calculate IOB Needed")
+                }
+                .font(.footnote)
+                .cornerRadius(40)
+                .padding(10)
+                .overlay(
+                RoundedRectangle(cornerRadius: 40)
+                    .stroke(Color.blue, lineWidth: 1))
+                Spacer()
             }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            HStack {
+                Text("Insulin on Board Needed")
+                Spacer()
+                Text(iobn)
             }
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
